6174a487b79f1f200d2ddfadbd905c1897c7b8f4
HamsterJesus <2014302@rgu.ac.uk>
2024-02-25 02:07:46 +0000

    yes Mama

--- new stuff/assets/imageClicks.js
+++ new stuff/assets/imageClicks.js
@@ -1,4 +1,10 @@
-function logToConsole(event){
-    event.preventDefault();
-    console.log("I've been clicked")
+// function logToConsole(event){
+//     event.preventDefault();
+//     console.log("I've been clicked")
+// }
+
+function markerChecked(marker){
+    if (marker === 1) {
+        document.getElementById("infoText").innerHTML = "<p>Your dad is geese</p>";
+    } 
 }
